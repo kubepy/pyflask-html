05d0d702ebbd9483c8c923b343bd06fa2a69ac0e
root <root@149.28.231.154.vultr.com>
2019-12-29 22:13:58 +0800

    update

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 #!/bin/python
 import flask
+from flask import request
 import subprocess
 import time
 
@@ -9,20 +10,23 @@ app = flask.Flask(__name__, static_url_path='', static_folder='',)
 def root():
     return app.send_static_file('static_web.html')
 
-@app.route('/api/start/ansible')
+@app.route('/api/start/ansible',methods=['POST'])
 def start():
-    def inner():
-        proc = subprocess.Popen(
-            ['ansible-playbook -i inventory site.yml'],
-            shell=True,
-            stdout=subprocess.PIPE
-        )
+    button = request.form.get('button')
 
-        for line in iter(proc.stdout.readline,''):
-            time.sleep(0.1) 
-            yield line.rstrip() + '<br/>\n'
-
-    return flask.Response(inner(), mimetype='text/html')
+    if request.method == "POST":
+        def inner():
+            proc = subprocess.Popen(
+                ['ansible-playbook -i inventory "{0}"'.format(button)],
+                shell=True,
+                stdout=subprocess.PIPE
+            )
+    
+            for line in iter(proc.stdout.readline,''):
+                time.sleep(0.1) 
+                yield line.rstrip() + '<br/>\n'
+    
+        return flask.Response(inner(), mimetype='text/html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
